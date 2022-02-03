e7dda98037bb33b873fcff45ac76ce58a15132c7
Max Neuvians <max.neuvians@cds-snc.ca>
2022-02-03 15:26:00 -0500

    feat: use read only role for plan

--- terraform/oidc_role.tf
+++ terraform/oidc_role.tf
@@ -1,27 +1,51 @@
 locals {
-  admin_role = "AWSSentinelConnectorLayerTerraformAdministratorRole"
+  plan_name  = "AWSSentinelConnectorLayerTerraformReadOnlyRole"
+  admin_name = "AWSSentinelConnectorLayerTerraformAdministratorRole"
 }
 
-# Role used by Terraform and GitHub actions
+data "aws_caller_identity" "current" {}
+
 module "gh_oidc_roles" {
-  source = "github.com/cds-snc/terraform-modules?ref=v1.0.3//gh_oidc_role"
+  source = "github.com/cds-snc/terraform-modules?ref=v1.0.6//gh_oidc_role"
   roles = [
     {
-      name      = local.admin_role
+      name      = local.plan_name
       repo_name = "aws-sentinel-connector-layer"
       claim     = "*"
+    },
+    {
+      name      = local.admin_name
+      repo_name = "aws-sentinel-connector-layer"
+      claim     = "ref:refs/heads/main"
     }
   ]
 
   billing_tag_value = var.billing_code
 }
 
+module "attach_tf_plan_policy" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v1.0.6//attach_tf_plan_policy"
+  account_id        = data.aws_caller_identity.current.account_id
+  role_name         = local.plan_name
+  bucket_name       = "aws-sentinel-connection-layer-tf"
+  lock_table_name   = "terraform-state-lock-dynamo"
+  billing_tag_value = var.billing_code
+  depends_on = [
+    module.gh_oidc_roles
+  ]
+}
+
 data "aws_iam_policy" "admin" {
   name = "AdministratorAccess"
+  depends_on = [
+    module.gh_oidc_roles
+  ]
 }
 
 resource "aws_iam_role_policy_attachment" "admin" {
-  role       = local.admin_role
+  role       = local.admin_name
   policy_arn = data.aws_iam_policy.admin.arn
-  depends_on = [module.gh_oidc_roles]
+  depends_on = [
+    module.gh_oidc_roles
+  ]
 }
