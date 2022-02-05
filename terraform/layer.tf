a0ed8f15ba3b1b12d471b598f0b05dbb9ce3856c
Max Neuvians <max.neuvians@cds-snc.ca>
2022-02-05 20:13:42 +0000

    fix: add layer permission

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -6,3 +6,11 @@ resource "aws_lambda_layer_version" "lambda_layer" {
 
   compatible_runtimes = ["python3.9"]
 }
+
+resource "aws_lambda_layer_version_permission" "lambda_layer_permission" {
+  layer_name     = aws_lambda_layer_version.lambda_layer.layer_name
+  version_number = aws_lambda_layer_version.lambda_layer.version_number
+  principal      = "*"
+  action         = "lambda:GetLayerVersion"
+  statement_id   = "shared-layer-permission"
+}
