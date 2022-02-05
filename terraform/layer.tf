d709a4f308574346e5c4e59304b40fc2ebbba351
Max Neuvians <max.neuvians@cds-snc.ca>
2022-02-05 20:15:36 +0000

    fix: version attr

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -8,8 +8,8 @@ resource "aws_lambda_layer_version" "lambda_layer" {
 }
 
 resource "aws_lambda_layer_version_permission" "lambda_layer_permission" {
-  layer_name     = aws_lambda_layer_version.lambda_layer.layer_name
-  version_number = aws_lambda_layer_version.lambda_layer.version_number
+  layer_name     = aws_lambda_layer_version.lambda_layer.arn
+  version_number = aws_lambda_layer_version.lambda_layer.version
   principal      = "*"
   action         = "lambda:GetLayerVersion"
   statement_id   = "shared-layer-permission"
