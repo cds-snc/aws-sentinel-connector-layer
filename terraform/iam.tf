e7a16be0123d3058551da297c8c5fee9aaf18d0b
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-05 16:38:06 +0000

    feat: add IAM role to list lambda layer versions
    Add an IAM role that can be used to list the Lambda layer versions.  This can be
    used during TF operations to dynamically use the latest available Sentinel
    Connector Lambda layer.

---
+++ terraform/iam.tf
@@ -0,0 +1,52 @@
+resource "aws_iam_role" "lambda_list_layers" {
+  name               = "lambda-list-layers"
+  assume_role_policy = data.aws_iam_policy_document.lambda_list_layers_assume.json
+}
+
+resource "aws_iam_policy" "lambda_list_layers" {
+  name        = "lambda-list-layers"
+  description = "Policy to allow listing Lambda layers and versions"
+  policy      = data.aws_iam_policy_document.lambda_list_layers.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_list_layers" {
+  role       = aws_iam_role.lambda_list_layers.name
+  policy_arn = aws_iam_policy.lambda_list_layers.arn
+}
+
+data "aws_iam_policy_document" "lambda_list_layers_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::239043911459:role/notification-terraform-apply",
+        "arn:aws:iam::239043911459:role/notification-terraform-plan",
+        "arn:aws:iam::296255494825:role/notification-terraform-apply",
+        "arn:aws:iam::296255494825:role/notification-terraform-plan",
+        "arn:aws:iam::800095993820:role/notification-terraform-apply",
+        "arn:aws:iam::800095993820:role/notification-terraform-plan",
+        "arn:aws:iam::891376947407:role/notification-terraform-apply",
+        "arn:aws:iam::891376947407:role/notification-terraform-plan",
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_list_layers" {
+  statement {
+    sid    = "ListLayers"
+    effect = "Allow"
+    actions = [
+      "lambda:GetFunction",
+      "lambda:ListLayers",
+      "lambda:ListLayerVersions"
+    ]
+    resources = ["*"]
+  }
+}
