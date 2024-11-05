37484ff185eb242b19d834a27e51432a01cb66ff
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-05 13:44:42 -0500

    feat: add IAM role to list lambda layer versions (#178)
    Add an IAM role that can be used to list the Lambda layer versions.  This can be
    used during TF operations to dynamically use the latest available Sentinel
    Connector Lambda layer.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "billing_code" {
   description = "The billing code to tag our resources with"
   type        = string
 }
+
+variable "lambda_list_layers_assume_principals" {
+  description = "The list of AWS principals role ARNs that can assume the lambda-list-layers role"
+  type        = list(string)
+  sensitive   = true
+}
