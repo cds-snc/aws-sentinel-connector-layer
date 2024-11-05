aa77f363445b0a437f88c9c421172ca3910f9e80
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-05 13:14:14 -0500

    feat: add principal role arns var

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "lambda_list_layers" {
   name               = "lambda-list-layers"
-  assume_role_policy = data.aws_iam_policy_document.lambda_list_layers_assume.json
+  assume_role_policy = sensitive(data.aws_iam_policy_document.lambda_list_layers_assume.json)
 }
 
 resource "aws_iam_policy" "lambda_list_layers" {
@@ -19,21 +19,18 @@ data "aws_iam_policy_document" "lambda_list_layers_assume" {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
     principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    principals {
       type = "AWS"
-      identifiers = [
-        "arn:aws:iam::239043911459:role/notification-terraform-apply",
-        "arn:aws:iam::239043911459:role/notification-terraform-plan",
-        "arn:aws:iam::296255494825:role/notification-terraform-apply",
-        "arn:aws:iam::296255494825:role/notification-terraform-plan",
-        "arn:aws:iam::800095993820:role/notification-terraform-apply",
-        "arn:aws:iam::800095993820:role/notification-terraform-plan",
-        "arn:aws:iam::891376947407:role/notification-terraform-apply",
-        "arn:aws:iam::891376947407:role/notification-terraform-plan",
-      ]
+      identifiers = concat(
+        var.lambda_list_layers_assume_principals,
+        [
+          "arn:aws:iam::239043911459:role/notification-terraform-apply",
+          "arn:aws:iam::239043911459:role/notification-terraform-plan",
+          "arn:aws:iam::296255494825:role/notification-terraform-apply",
+          "arn:aws:iam::296255494825:role/notification-terraform-plan",
+          "arn:aws:iam::800095993820:role/notification-terraform-apply",
+          "arn:aws:iam::800095993820:role/notification-terraform-plan",
+        ]
+      )
     }
   }
 }
